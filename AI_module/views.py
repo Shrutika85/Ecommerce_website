4180c79b183071daa38d825916ba5aaa334e095f
Shrey63 <shreyaparasagrawal@gmail.com>
2022-03-22 22:09:17 +0530

    frontend integration Almost done

--- AI_module/views.py
+++ AI_module/views.py
@@ -9,7 +9,7 @@ from django.db import connection
 
 def home(request):
     context={'a': 1}
-    return render(request, 'sign-in.html', {'name': 'Shreya'})
+    return render(request, 'footer.html', {'name': 'Shreya'})
 
 def getimage(request):
     # with connection.cursor() as cursor:
@@ -18,9 +18,9 @@ def getimage(request):
     #     print(row)
     #return render(request, 'image.html', context)
         print(request)
-        fileobj=request.FILES['filepath']
-        fs=FileSystemStorage()
-        filePathName=fs.save(fileobj.name, fileobj)
-        filePathName=fs.url(filePathName)
-        context = {'filePathName': filePathName}
-        return render(request, 'image.html', context)
+        # fileobj=request.FILES['filepath']
+        # fs=FileSystemStorage()
+        # filePathName=fs.save(fileobj.name, fileobj)
+        # filePathName=fs.url(filePathName)
+        # context = {'filePathName': filePathName}
+        return render(request, 'index.html',)
