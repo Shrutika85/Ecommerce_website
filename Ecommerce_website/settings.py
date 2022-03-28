c15e3a255c9a32f1b1f4eb4df1cae73cb88f14d1
Shrutika85 <mahajanshrutika8@gmail.com>
2022-03-28 18:39:42 +0530

    navbar inserted

--- Ecommerce_website/settings.py
+++ Ecommerce_website/settings.py
@@ -73,20 +73,20 @@ WSGI_APPLICATION = 'Ecommerce_website.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-         'ENGINE': 'django.db.backends.mysql',
-         'NAME': 'clothifyy_db',
-        'USER': 'root',
-        'HOST': 'localhost',
-        'PORT': 3306,
-        'PASSWORD': '',
-'OPTIONS': {
-'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-    }
-}
-}
+#
+# DATABASES = {
+#     'default': {
+#          'ENGINE': 'django.db.backends.mysql',
+#          'NAME': 'clothifyy_db',
+#         'USER': 'root',
+#         'HOST': 'localhost',
+#         'PORT': 3306,
+#         'PASSWORD': '',
+# 'OPTIONS': {
+# 'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+#     }
+# }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
